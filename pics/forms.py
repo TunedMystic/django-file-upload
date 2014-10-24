ff83867c819dfc90b81fe30bd9f54723829d642b
Sandeep <jadoonanansandeep@gmail.com>
2014-10-23 20:32:30 -0400

    Work on 'uploadFile' view.

--- pics/forms.py
+++ pics/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 from pics.models import PictureUpload
 
@@ -6,3 +7,7 @@ class PictureUploadForm(forms.ModelForm):
    
   class Meta:
     model = PictureUpload
+    exclude = []
+    labels = {
+      "img": _("Image")
+    }
--- pics/views.py
+++ pics/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.core.urlresolvers import reverse
 
+from pics.models import PictureUpload
 from pics.forms import PictureUploadForm
 
 # Create your views here.
 def uploadFile(request):
-  return HttpResponse("This is the uploads page.")
+  """
+  Displays an upload form.
+  """
+  
+  # If a form was POSTed.
+  if request.method == "POST":
+    # Instantiate a ModelForm with the request data.
+    uploadForm = PictureUploadForm(request.POST, request.FILES)
+    if uploadForm.is_valid():
+      # Save the form data (the picture).
+      uploadForm.save()
+      return HttpResponseRedirect(reverse("index"))
+   
+  # No form data, so return an empty form.
+  else:
+    uploadForm = PictureUploadForm()
+    
+  return render(request, "pics/uploadForm.html", {"form": uploadForm})
 
 
 def listFiles(request):
+  """
+  Displays a list of all uploaded images.
+  """
+  
   return HttpResponse("This is the list files page.")
